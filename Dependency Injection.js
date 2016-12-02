14271c23567720f7920f686f601c022b883570d4
Kyle Meier <kyledmeier@gmail.com>
2016-12-02 15:26:41 -0800

    dependency injection

---
+++ Dependency Injection.js
@@ -0,0 +1,22 @@
+class DI{
+  constructor(dependency){
+    this.dependency = dependency;
+  }
+  
+  inject(func){
+  
+    let args = /\(.*(?=\))/.exec( func.toString() )[0];
+    
+    args = args.slice(1).split(/\s?,\s?/)
+
+    const dependencies = args.reduce( (result, arg) =>{
+    
+      const item = this.dependency[arg];
+      if( item ) result.push(item);
+      
+      return result;
+    },[]);
+  
+    return func.bind(null, ...dependencies);
+  }
+}
